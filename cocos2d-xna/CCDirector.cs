a176e54f67fd890186122cef35b656490fa8ac66
ElvisQin <elvis@openxlive.com>
2011-10-26 02:32:06 +0000

    可能checkout项目导致其他账户不能提交，commit一下

--- cocos2d-xna/CCDirector.cs
+++ cocos2d-xna/CCDirector.cs
@@ -11,37 +11,107 @@ using Microsoft.Xna.Framework.Media;
 
 namespace cocos2d
 {
-    /// <summary>
-    /// This is a game component that implements IUpdateable.
-    /// </summary>
-    public class CCDirector : Microsoft.Xna.Framework.GameComponent
+    public class CCDirector
     {
-        public CCDirector(Game game)
-            : base(game)
-        {
-            // TODO: Construct any child components here
-        }
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
-        public override void Initialize()
-        {
-            // TODO: Add your initialization code here
+    }
 
-            base.Initialize();
-        }
+    /// <summary>
+    ///  Possible OpenGL projections used by director
+    /// </summary>
+    enum ccDirectorProjection
+    {
+        /// sets a 2D projection (orthogonal projection)
+        kCCDirectorProjection2D,
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
-            // TODO: Add your update code here
+        /// sets a 3D projection with a fovy=60, znear=0.5f and zfar=1500.
+        kCCDirectorProjection3D,
 
-            base.Update(gameTime);
-        }
+        /// it calls "updateProjection" on the projection delegate.
+        kCCDirectorProjectionCustom,
+
+        /// Detault projection is 3D projection
+        kCCDirectorProjectionDefault = kCCDirectorProjection3D,
+
+        // backward compatibility stuff
+        CCDirectorProjection2D = kCCDirectorProjection2D,
+        CCDirectorProjection3D = kCCDirectorProjection3D,
+        CCDirectorProjectionCustom = kCCDirectorProjectionCustom
+    }
+
+    /// <summary>
+    /// Possible Director Types.
+    /// since v0.8.2
+    /// </summary>
+    enum ccDirectorType
+    {
+        /** Will use a Director that triggers the main loop from an NSTimer object
+         *
+         * Features and Limitations:
+         * - Integrates OK with UIKit objects
+         * - It the slowest director
+         * - The interval update is customizable from 1 to 60
+         */
+        kCCDirectorTypeNSTimer,
+
+        /** will use a Director that triggers the main loop from a custom main loop.
+         *
+         * Features and Limitations:
+         * - Faster than NSTimer Director
+         * - It doesn't integrate well with UIKit objects
+         * - The interval update can't be customizable
+         */
+        kCCDirectorTypeMainLoop,
+
+        /** Will use a Director that triggers the main loop from a thread, but the main loop will be executed on the main thread.
+         *
+         * Features and Limitations:
+         * - Faster than NSTimer Director
+         * - It doesn't integrate well with UIKit objects
+         * - The interval update can't be customizable
+         */
+        kCCDirectorTypeThreadMainLoop,
+
+        /** Will use a Director that synchronizes timers with the refresh rate of the display.
+         *
+         * Features and Limitations:
+         * - Faster than NSTimer Director
+         * - Only available on 3.1+
+         * - Scheduled timers & drawing are synchronizes with the refresh rate of the display
+         * - Integrates OK with UIKit objects
+         * - The interval update can be 1/60, 1/30, 1/15
+         */
+        kCCDirectorTypeDisplayLink,
+
+        /** Default director is the NSTimer directory */
+        kCCDirectorTypeDefault = kCCDirectorTypeNSTimer,
+
+        // backward compatibility stuff
+        CCDirectorTypeNSTimer = kCCDirectorTypeNSTimer,
+        CCDirectorTypeMainLoop = kCCDirectorTypeMainLoop,
+        CCDirectorTypeThreadMainLoop = kCCDirectorTypeThreadMainLoop,
+        CCDirectorTypeDisplayLink = kCCDirectorTypeDisplayLink,
+        CCDirectorTypeDefault = kCCDirectorTypeDefault
+    }
+
+    /// <summary>
+    /// Possible device orientations
+    /// </summary>
+    enum ccDeviceOrientation
+    {
+        /// Device oriented vertically, home button on the bottom
+        kCCDeviceOrientationPortrait = 0, // UIDeviceOrientationPortrait,	
+        /// Device oriented vertically, home button on the top
+        kCCDeviceOrientationPortraitUpsideDown = 1, // UIDeviceOrientationPortraitUpsideDown,
+        /// Device oriented horizontally, home button on the right
+        kCCDeviceOrientationLandscapeLeft = 2, // UIDeviceOrientationLandscapeLeft,
+        /// Device oriented horizontally, home button on the left
+        kCCDeviceOrientationLandscapeRight = 3, // UIDeviceOrientationLandscapeRight,
+
+        // Backward compatibility stuff
+        CCDeviceOrientationPortrait = kCCDeviceOrientationPortrait,
+        CCDeviceOrientationPortraitUpsideDown = kCCDeviceOrientationPortraitUpsideDown,
+        CCDeviceOrientationLandscapeLeft = kCCDeviceOrientationLandscapeLeft,
+        CCDeviceOrientationLandscapeRight = kCCDeviceOrientationLandscapeRight
     }
 }
