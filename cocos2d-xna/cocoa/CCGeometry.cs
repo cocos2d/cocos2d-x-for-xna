9b8ba3db1c503855c5b7909622afd1fcb8ec2d31
minggo <ming@cocos2d-x.org>
2011-10-26 05:37:30 +0000

    issue #770: add CCGeometry.cs

--- cocos2d-xna/cocoa/CCGeometry.cs
+++ cocos2d-xna/cocoa/CCGeometry.cs
@@ -22,9 +22,138 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
 
+using System;
 namespace cocos2d
 {
-    class CCGeometry
+    public class CCPoint
     {
+        public float x;
+        public float y;
+
+        public CCPoint()
+        {
+            x = 0.0f;
+            y = 0.0f;
+        }
+
+        public CCPoint(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool CCPointEqualToPoint(CCPoint point1, CCPoint point2)
+        {
+            if ((point1 == null) || (point2 == null))
+            {
+                return false;
+            }
+            else
+            {
+                return ((point1.x == point2.x) && (point1.y == point2.y));
+            }
+        }
+    }
+
+    public class CCSize
+    {
+        public float width;
+        public float height;
+
+        public CCSize()
+        {
+            width = 0.0f;
+            height = 0.0f;
+        }
+
+        public CCSize(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static bool CCSizeEqualSize(CCSize size1, CCSize size2)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CCRect
+    {
+        public CCPoint origin;
+        public CCSize size;
+
+        public CCRect()
+        {
+            throw new NotImplementedException();
+            ///@todo initialization
+        }
+
+        public CCRect(float x, float y, float width, float height)
+        {
+            throw new NotImplementedException();
+            ///@todo initialization
+        }
+
+        // return the leftmost x-value of 'rect'
+        public static float CCRectGetMinX(CCRect rect)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // return the rightmost x-value of 'rect'
+        public static float CCRectGetMaxX(CCRect rect)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // return the midpoint x-value of 'rect'
+        public static float CCRectGetMidX(CCRect rect)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // Return the bottommost y-value of 'rect'
+        public static float CCRectGetMinY(CCRect rect)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // Return the topmost y-value of 'rect'
+        public static float CCRectGetMaxY(CCRect rect)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // Return the midpoint y-value of 'rect'
+        public static float CCRectGetMidY(CCRect rect)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        public static bool CCRectEqualToRect(CCRect rect1, CCRect rect2)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        public static bool CCRectContainsPoint(CCRect rect1, CCRect rect2)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        public static bool CCRectIntersetsRect(CCRect rect1, CCRect rect2)
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
     }
 }
