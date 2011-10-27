c153c2bafe5158be501a05d2db9a16f6d9609539
ElvisQin <elvis@openxlive.com>
2011-10-27 03:04:08 +0000

    issue #771：Add CCDisplayLinkDirector

---
+++ cocos2d-xna/CCDisplayLinkDirector.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cocos2d
+{
+    public class CCDisplayLinkDirector : CCDirector
+    {
+        public override void setAnimationInterval(double dValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void stopAnimation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void startAnimation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void mainLoop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
