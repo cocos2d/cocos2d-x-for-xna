f790eaf51dd1df4bace5760b4bbf353baef1180d
flyingpacer <flyingpacer@gmail.com>
2011-10-27 06:11:25 +0000

    issue #774 implement cctypes

--- cocos2d-xna/predefine/ccTypes.cs
+++ cocos2d-xna/predefine/ccTypes.cs
@@ -35,272 +35,234 @@ using System.Text;
 namespace   cocos2d 
 {
 
-    ///** RGB color composed of bytes 3 bytes
-    //@since v0.8
-    // */
-    //public struct ccColor3B
-    //{
-    //    byte	r;
-    //    byte	g;
-    //    byte    b;
-    //};
+    /** RGB color composed of bytes 3 bytes
+    @since v0.8
+     */
+    public struct ccColor3B
+    {
+        public ccColor3B(byte inr, byte ing, byte inb)
+        {
+            r = inr;
+            g = ing;
+            b = inb;
+        }
 
-    ////! helper macro that creates an ccColor3B type
-    //public static ccColor3B ccc3(byte r, byte g, byte b)
-    //{
-    //    ccColor3B c = {r, g, b};
-    //    return c;
-    //}
-    ////ccColor3B predefined colors
-    ////! White color (255,255,255)
-    //public static const ccColor3B ccWHITE={255,255,255};
-    ////! Yellow color (255,255,0)
-    //public static const ccColor3B ccYELLOW={255,255,0};
-    ////! Blue color (0,0,255)
-    //public static const ccColor3B ccBLUE={0,0,255};
-    ////! Green Color (0,255,0)
-    //public static const ccColor3B ccGREEN={0,255,0};
-    ////! Red Color (255,0,0,)
-    //public static const ccColor3B ccRED={255,0,0};
-    ////! Magenta Color (255,0,255)
-    //public static const ccColor3B ccMAGENTA={255,0,255};
-    ////! Black Color (0,0,0)
-    //public static const ccColor3B ccBLACK={0,0,0};
-    ////! Orange Color (255,127,0)
-    //public static const ccColor3B ccORANGE={255,127,0};
-    ////! Gray Color (166,166,166)
-    //public static const ccColor3B ccGRAY={166,166,166};
+        public byte r;
+        public byte g;
+        public byte b;
+    };
 
-    ///** RGBA color composed of 4 bytes
-    //@since v0.8
-    //*/
-    //public struct ccColor4B
-    //{
-    //    byte r;
-    //    byte g;
-    //    byte b;
-    //    byte a;
-    //};
-    ////! helper macro that creates an ccColor4B type
-    //public static ccColor4B ccc4(byte r, byte g, byte b, byte o)
-    //{
-    //    ccColor4B c = {r, g, b, o};
-    //    return c;
-    //}
+    /** RGBA color composed of 4 bytes
+    @since v0.8
+    */
+    public struct ccColor4B
+    {
+        public ccColor4B(byte inr, byte ing, byte inb, byte ina)
+        {
+            r = inr;
+            g = ing;
+            b = inb;
+            a = ina;
+        }
+
+        public byte r;
+        public byte g;
+        public byte b;
+        public byte a;
+    };
     
-    ///** RGBA color composed of 4 floats
-    //@since v0.8
-    //*/
-    //public struct ccColor4F {
-    //    float r;
-    //    float g;
-    //    float b;
-    //    float a;
-    //};
+    /** RGBA color composed of 4 floats
+    @since v0.8
+    */
+    public struct ccColor4F {
+        public ccColor4F(float inr, float ing, float inb, float ina)
+        {
+            r = inr;
+            g = ing;
+            b = inb;
+            a = ina;
+        }
 
-    ///** Returns a ccColor4F from a ccColor3B. Alpha will be 1.
-    // @since v0.99.1
-    // */
-    //public static ccColor4F ccc4FFromccc3B(ccColor3B c)
-    //{
-    //    ccColor4F c4 = {c.r/255.f, c.g/255.f, c.b/255.f, 1.f};
-    //    return c4;
-    //}
+        public float r;
+        public float g;
+        public float b;
+        public float a;
+    };
 
-    ///** Returns a ccColor4F from a ccColor4B.
-    // @since v0.99.1
-    // */
-    //public static ccColor4F ccc4FFromccc4B(ccColor4B c)
-    //{
-    //    ccColor4F c4 = {c.r/255.f, c.g/255.f, c.b/255.f, c.a/255.f};
-    //    return c4;
-    //}
+    /** A vertex composed of 2 floats: x, y
+     @since v0.8
+     */
+    public struct ccVertex2F
+    {
+        public ccVertex2F(float inx, float iny)
+        {
+            x = inx;
+            y = iny;
+        }
 
-    ///** returns YES if both ccColor4F are equal. Otherwise it returns NO.
-    // @since v0.99.1
-    // */
-    //public static bool ccc4FEqual(ccColor4F a, ccColor4F b)
-    //{
-    //    return a.r == b.r && a.g == b.g && a.b == b.b && a.a == b.a;
-    //}
+        public float x;
+        public float y;
+    };
 
-    ///** A vertex composed of 2 floats: x, y
-    // @since v0.8
-    // */
-    //public struct ccVertex2F
-    //{
-    //    float x;
-    //    float y;
-    //};
+    /** A vertex composed of 2 floats: x, y
+     @since v0.8
+     */
+    public struct ccVertex3F
+    {
+        public ccVertex3F(float inx, float iny, float inz)
+        {
+            x = inx;
+            y = iny;
+            z = inz;
+        }
 
-    //public static ccVertex2F vertex2(const float x, const float y)
-    //{
-    //    ccVertex2F c = {x, y};
-    //    return c;
-    //}
+        public float x;
+        public float y;
+        public float z;
+    };
+     /** A texcoord composed of 2 floats: u, y
+     @since v0.8
+     */
+    public struct ccTex2F 
+    {
+        public ccTex2F(float inu, float inv)
+        {
+            u = inu;
+            v = inv;
+        }
 
+        public float u;
+        public float v;
+    };
 
-    ///** A vertex composed of 2 floats: x, y
-    // @since v0.8
-    // */
-    //public struct ccVertex3F
-    //{
-    //    float x;
-    //    float y;
-    //    float z;
-    //};
+    //! Point Sprite component
+    public struct ccPointSprite
+    {
+        public ccVertex2F pos;		// 8 bytes
+        public ccColor4B color;		// 4 bytes
+        public float size;		// 4 bytes
+    };
 
-    //public static ccVertex3F vertex3(const float x, const float y, const float z)
-    //{
-    //    ccVertex3F c = {x, y, z};
-    //    return c;
-    //}
-		
-    ///** A texcoord composed of 2 floats: u, y
-    // @since v0.8
-    // */
-    //public struct ccTex2F 
-    //{
-    //     float u;
-    //     float v;
-    //};
-
-    //public static ccTex2F tex2(const float u, const float v)
-    //{
-    //    ccTex2F t = {u , v};
-    //    return t;
-    //}
-
- 
-    ////! Point Sprite component
-    //public struct ccPointSprite
-    //{
-    //    ccVertex2F	pos;		// 8 bytes
-    //    ccColor4B	color;		// 4 bytes
-    //    float		size;		// 4 bytes
-    //};
-
-    ////!	A 2D Quad. 4 * 2 floats
-    //public struct ccQuad2 
-    //{
-    //    ccVertex2F		tl;
-    //    ccVertex2F		tr;
-    //    ccVertex2F		bl;
-    //    ccVertex2F		br;
-    //};
+    //!	A 2D Quad. 4 * 2 floats
+    public struct ccQuad2 
+    {
+        public ccVertex2F tl;
+        public ccVertex2F tr;
+        public ccVertex2F bl;
+        public ccVertex2F br;
+    };
 
 
-    ////!	A 3D Quad. 4 * 3 floats
-    //public struct ccQuad3 {
-    //    ccVertex3F		bl;
-    //    ccVertex3F		br;
-    //    ccVertex3F		tl;
-    //    ccVertex3F		tr;
-    //};
+    //!	A 3D Quad. 4 * 3 floats
+    public struct ccQuad3 {
+        public ccVertex3F bl;
+        public ccVertex3F br;
+        public ccVertex3F tl;
+        public ccVertex3F tr;
+    };
 
-    ////! A 2D grid size
-    //public struct ccGridSize
-    //{
-    //    int	x;
-    //    int	y;
-    //};
+    //! A 2D grid size
+    public struct ccGridSize
+    {
+        public ccGridSize(int inx, int iny)
+        {
+            x = inx;
+            y = iny;
+        }
 
-    ////! helper function to create a ccGridSize
-    //public static ccGridSize ccg(const int x, const int y)
-    //{
-    //    ccGridSize v = {x, y};
-    //    return v;
-    //}
+        public int x;
+        public int y;
+    };
 
-    ////! a Point with a vertex point, a tex coord point and a color 4B
-    //public struct ccV2F_C4B_T2F
-    //{
-    //    //! vertices (2F)
-    //    ccVertex2F		vertices;
-    //    //! colors (4B)
-    //    ccColor4B		colors;
-    //    //! tex coords (2F)
-    //    ccTex2F			texCoords;
-    //};
+        //! a Point with a vertex point, a tex coord point and a color 4B
+    public struct ccV2F_C4B_T2F
+    {
+        //! vertices (2F)
+        public ccVertex2F vertices;
+        //! colors (4B)
+        public ccColor4B colors;
+        //! tex coords (2F)
+        public ccTex2F texCoords;
+    };
 
-    ////! a Point with a vertex point, a tex coord point and a color 4F
-    //public struct ccV2F_C4F_T2F
-    //{
-    //    //! vertices (2F)
-    //    ccVertex2F		vertices;
-    //    //! colors (4F)
-    //    ccColor4F		colors;
-    //    //! tex coords (2F)
-    //    ccTex2F			texCoords;
-    //};
+    //! a Point with a vertex point, a tex coord point and a color 4F
+    public struct ccV2F_C4F_T2F
+    {
+        //! vertices (2F)
+        public ccVertex2F vertices;
+        //! colors (4F)
+        public ccColor4F colors;
+        //! tex coords (2F)
+        public ccTex2F texCoords;
+    };
 
-    ////! a Point with a vertex point, a tex coord point and a color 4B
-    //public struct ccV3F_C4B_T2F
-    //{
-    //    //! vertices (3F)
-    //    ccVertex3F		vertices;			// 12 bytes
-    ////	char __padding__[4];
+    //! a Point with a vertex point, a tex coord point and a color 4B
+    public struct ccV3F_C4B_T2F
+    {
+        //! vertices (3F)
+        public ccVertex3F vertices;			// 12 bytes
+    //	char __padding__[4];
 
-    //    //! colors (4B)
-    //    ccColor4B		colors;				// 4 bytes
-    ////	char __padding2__[4];
+        //! colors (4B)
+        public ccColor4B colors;				// 4 bytes
+    //	char __padding2__[4];
 
-    //    // tex coords (2F)
-    //    ccTex2F			texCoords;			// 8 byts
-    //} ;
+        // tex coords (2F)
+        public ccTex2F texCoords;			// 8 byts
+    } ;
 
-    ////! 4 ccVertex2FTex2FColor4B Quad
-    //public struct ccV2F_C4B_T2F_Quad
-    //{
-    //    //! bottom left
-    //    ccV2F_C4B_T2F	bl;
-    //    //! bottom right
-    //    ccV2F_C4B_T2F	br;
-    //    //! top left
-    //    ccV2F_C4B_T2F	tl;
-    //    //! top right
-    //    ccV2F_C4B_T2F	tr;
-    //} ;
+    //! 4 ccVertex2FTex2FColor4B Quad
+    public struct ccV2F_C4B_T2F_Quad
+    {
+        //! bottom left
+        public ccV2F_C4B_T2F bl;
+        //! bottom right
+        public ccV2F_C4B_T2F br;
+        //! top left
+        public ccV2F_C4B_T2F tl;
+        //! top right
+        public ccV2F_C4B_T2F tr;
+    } ;
 
-    ////! 4 ccVertex3FTex2FColor4B
-    //public struct ccV3F_C4B_T2F_Quad
-    //{
-    //    //! top left
-    //    ccV3F_C4B_T2F	tl;
-    //    //! bottom left
-    //    ccV3F_C4B_T2F	bl;
-    //    //! top right
-    //    ccV3F_C4B_T2F	tr;
-    //    //! bottom right
-    //    ccV3F_C4B_T2F	br;
-    //} ;
+    //! 4 ccVertex3FTex2FColor4B
+    public struct ccV3F_C4B_T2F_Quad
+    {
+        //! top left
+        public ccV3F_C4B_T2F tl;
+        //! bottom left
+        public ccV3F_C4B_T2F bl;
+        //! top right
+        public ccV3F_C4B_T2F tr;
+        //! bottom right
+        public ccV3F_C4B_T2F br;
+    } ;
 
-    ////! 4 ccVertex2FTex2FColor4F Quad
-    //public struct ccV2F_C4F_T2F_Quad
-    //{
-    //    //! bottom left
-    //    ccV2F_C4F_T2F	bl;
-    //    //! bottom right
-    //    ccV2F_C4F_T2F	br;
-    //    //! top left
-    //    ccV2F_C4F_T2F	tl;
-    //    //! top right
-    //    ccV2F_C4F_T2F	tr;
-    //} ;
+    //! 4 ccVertex2FTex2FColor4F Quad
+    public struct ccV2F_C4F_T2F_Quad
+    {
+        //! bottom left
+        public ccV2F_C4F_T2F bl;
+        //! bottom right
+        public ccV2F_C4F_T2F br;
+        //! top left
+        public ccV2F_C4F_T2F tl;
+        //! top right
+        public ccV2F_C4F_T2F tr;
+    } ;
 
-    ////! Blend Function used for textures
-    //public struct ccBlendFunc
-    //{
-    //    //! source blend function
-    //    uint src;
-    //    //! destination blend function
-    //    uint dst;
-    //};
+    //! Blend Function used for textures
+    public struct ccBlendFunc
+    {
+        //! source blend function
+        public uint src;
+        //! destination blend function
+        public uint dst;
+    };
 
-    ////! delta time type
-    ////! if you want more resolution redefine it as a double
-    //#define ccTime float
-    ////typedef double ccTime;
+    //! delta time type
+    //! if you want more resolution redefine it as a double
+    // #define ccTime float
+    //typedef double ccTime;
 
     public enum CCTextAlignment
     {
@@ -308,6 +270,94 @@ namespace   cocos2d
 	    CCTextAlignmentCenter,
 	    CCTextAlignmentRight,
     };
+
+    public class ccTypes
+    {
+        //ccColor3B predefined colors
+        //! White color (255,255,255)
+        public readonly ccColor3B ccWHITE = new ccColor3B(255,255,255);
+        //! Yellow color (255,255,0)
+        public readonly ccColor3B ccYELLOW = new ccColor3B(255, 255, 0);
+        //! Blue color (0,0,255)
+        public readonly ccColor3B ccBLUE = new ccColor3B(0, 0, 255);
+        //! Green Color (0,255,0)
+        public readonly ccColor3B ccGREEN = new ccColor3B(0, 255, 0);
+        //! Red Color (255,0,0,)
+        public readonly ccColor3B ccRED = new ccColor3B(255, 0, 0);
+        //! Magenta Color (255,0,255)
+        public readonly ccColor3B ccMAGENTA = new ccColor3B(255, 0, 255);
+        //! Black Color (0,0,0)
+        public readonly ccColor3B ccBLACK = new ccColor3B(0, 0, 0);
+        //! Orange Color (255,127,0)
+        public readonly ccColor3B ccORANGE = new ccColor3B(255, 127, 0);
+        //! Gray Color (166,166,166)
+        public readonly ccColor3B ccGRAY = new ccColor3B(166, 166, 166);
+
+        //! helper macro that creates an ccColor3B type
+        static public ccColor3B ccc3(byte r, byte g, byte b)
+        {
+            ccColor3B c = new ccColor3B(r, g, b);
+            return c;
+        }
+
+        //! helper macro that creates an ccColor4B type
+        public static ccColor4B ccc4(byte r, byte g, byte b, byte o)
+        {
+            ccColor4B c = new ccColor4B(r, g, b, o);
+            return c;
+        }
+
+        /** Returns a ccColor4F from a ccColor3B. Alpha will be 1.
+         @since v0.99.1
+         */
+        public static ccColor4F ccc4FFromccc3B(ccColor3B c)
+        {
+            ccColor4F c4 = new ccColor4F(c.r/255.0f, c.g/255.0f, c.b/255.0f, 1.0f);
+            return c4;
+        }
+
+        /** Returns a ccColor4F from a ccColor4B.
+         @since v0.99.1
+         */
+        public static ccColor4F ccc4FFromccc4B(ccColor4B c)
+        {
+            ccColor4F c4 = new ccColor4F(c.r/255.0f, c.g/255.0f, c.b/255.0f, c.a/255.0f);
+            return c4;
+        }
+
+        /** returns YES if both ccColor4F are equal. Otherwise it returns NO.
+         @since v0.99.1
+         */
+        public static bool ccc4FEqual(ccColor4F a, ccColor4F b)
+        {
+            return a.r == b.r && a.g == b.g && a.b == b.b && a.a == b.a;
+        }
+
+        public static ccVertex2F vertex2(float x, float y)
+        {
+            ccVertex2F c = new ccVertex2F(x, y);
+            return c;
+        }
+
+        public static ccVertex3F vertex3(float x, float y, float z)
+        {
+            ccVertex3F c = new ccVertex3F(x, y, z);
+            return c;
+        }
+		
+        public static ccTex2F tex2(float u, float v)
+        {
+            ccTex2F t = new ccTex2F(u , v);
+            return t;
+        }
+
+        //! helper function to create a ccGridSize
+        public static ccGridSize ccg(int x, int y)
+        {
+            ccGridSize v = new ccGridSize(x, y);
+            return v;
+        }
+    }
 
 }//namespace   cocos2d 
 
