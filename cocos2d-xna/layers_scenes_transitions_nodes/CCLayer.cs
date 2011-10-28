19fede5a5405150501e3133c19de78324566377f
minggo <ming@cocos2d-x.org>
2011-10-28 07:46:24 +0000

    issue #767:set up skeleton of CCLayer

--- cocos2d-xna/layers_scenes_transitions_nodes/CCLayer.cs
+++ cocos2d-xna/layers_scenes_transitions_nodes/CCLayer.cs
@@ -24,6 +24,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 namespace cocos2d
 {
     //
@@ -35,7 +38,150 @@ namespace cocos2d
     - It can receive iPhone Touches
     - It can receive Accelerometer input
     */
-    public class CCLayer : CCNode
+    public class CCLayer : CCNode, CCTargetedTouchDelegate, CCStandardTouchDelegate
     {
+        public CCLayer()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        ~CCLayer()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        public static new CCLayer node()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        public override void onEnter()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+        public override void onExit()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+        public override void onEnterTransitionDidFinish()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // touches
+
+        public virtual bool ccTouchBegan(CCTouch touch, CCEvent event_)
+        {
+            Debug.Assert(false, "Layer#ccTouchBegan override me");
+            return true;
+        }
+        public virtual void ccTouchMoved(CCTouch touch, CCEvent event_)
+        {
+        }
+        public virtual void ccTouchEnded(CCTouch touch, CCEvent event_)
+        {
+        }
+        public virtual void ccTouchCancelled(CCTouch touch, CCEvent event_)
+        {
+        }
+        public virtual void ccTouchesBegan(List<CCTouch> touches, CCEvent event_)
+        {
+        }
+        public virtual void ccTouchesMoved(List<CCTouch> touches, CCEvent event_)
+        {
+        }
+        public virtual void ccTouchesEnded(List<CCTouch> touches, CCEvent event_)
+        {
+        }
+        public virtual void ccTouchesCancelled(List<CCTouch> touches, CCEvent event_)
+        {
+        }
+
+        /// <summary>
+        /// @todo
+        /// </summary>
+        //virtual void didAccelerate(CCAcceleration* pAccelerationValue) {CC_UNUSED_PARAM(pAccelerationValue);}
+
+        /** If isTouchEnabled, this method is called onEnter. Override it to change the
+	    way CCLayer receives touch events.
+	    ( Default: CCTouchDispatcher::sharedDispatcher()->addStandardDelegate(this,0); )
+	    Example:
+	    void CCLayer::registerWithTouchDispatcher()
+	    {
+	    CCTouchDispatcher::sharedDispatcher()->addTargetedDelegate(this,INT_MIN+1,true);
+	    }
+	    @since v0.8.0
+	    */
+        public virtual void registerWithTouchDispatcher()
+        {
+            ///@todo
+            throw new NotImplementedException();
+        }
+
+        // Properties
+
+        /** whether or not it will receive Touch events.
+	    You can enable / disable touch events with this property.
+	    Only the touches of this node will be affected. This "method" is not propagated to it's children.
+	    @since v0.8.1
+	    */
+        private bool m_bIsTouchEnabled;
+        public bool isTouchEnabled
+        {
+            get
+            {
+                ///@todo
+                throw new NotImplementedException();
+            }
+            set
+            {
+                ///@todo
+                throw new NotImplementedException();
+            }
+        }
+
+        /** whether or not it will receive Accelerometer events
+	    You can enable / disable accelerometer events with this property.
+	    @since v0.8.1
+	    */
+        private bool m_bIsAccelerometerEnabled;
+        public bool isAccelerometerEnabled
+        {
+            get
+            {
+                ///@todo
+                throw new NotImplementedException();
+            }
+            set
+            {
+                ///@todo
+                throw new NotImplementedException();
+            }
+        }
+
+        /** whether or not it will receive keypad events
+        You can enable / disable accelerometer events with this property.
+        it's new in cocos2d-x
+        */
+        private bool m_bIsKeypadEnabled;
+        public bool isKeypadEnabled
+        {
+            get
+            {
+                ///@todo
+                throw new NotImplementedException();
+            }
+            set
+            {
+                ///@todo
+                throw new NotImplementedException();
+            }
+        }
     }
 }
