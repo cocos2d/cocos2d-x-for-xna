96a38d01e20de397d015ae620adda63863293f1c
minggo <ming@cocos2d-x.org>
2011-10-28 06:58:38 +0000

    fixed #782: implement CCTouchDelegateProtocol

--- cocos2d-xna/predefine/CCTouchDelegateProtocol.cs
+++ cocos2d-xna/predefine/CCTouchDelegateProtocol.cs
@@ -23,6 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
 
+using System.Collections.Generic;
 namespace cocos2d
 {
     public interface CCTargetedTouchDelegate
@@ -31,9 +32,23 @@ namespace cocos2d
             @since v0.8
         */
         bool ccTouchBegan(CCTouch touch, CCEvent event_);
+        // touch updates
+        void ccTouchMoved(CCTouch touch, CCEvent event_);
+        void ccTouchEnded(CCTouch touch, CCEvent event_);
+        void ccTouchCancelled(CCTouch touch, CCEvent event_);
     }
 
+    /**
+     CCStandardTouchDelegate.
+ 
+     This type of delegate is the same one used by CocoaTouch. You will receive all the events (Began,Moved,Ended,Cancelled).
+     @since v0.8
+    */
     public interface CCStandardTouchDelegate
     {
+        void ccTouchesBegan(List<CCTouch> touches, CCEvent event_);
+        void ccTouchesMoved(List<CCTouch> touches, CCEvent event_);
+        void ccTouchesEnded(List<CCTouch> touches, CCEvent event_);
+        void ccTouchesCancelled(List<CCTouch> touches, CCEvent event_);
     }
 }
