92b0a8d774854eafb9d39477b373051b7c55292c
minggo <ming@cocos2d-x.org>
2011-11-03 03:48:02 +0000

    fixed #788: implement CCFollow

--- cocos2d-xna/actions/CCFollow.cs
+++ cocos2d-xna/actions/CCFollow.cs
@@ -24,9 +24,173 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
 
+using System.Diagnostics;
 namespace cocos2d
 {
     public class CCFollow : CCAction
     {
+        public CCFollow() { }
+
+        ~CCFollow() { }
+
+        public bool initWithTarget(CCNode followedNode, CCRect rect)
+        {
+            Debug.Assert(followedNode != null);
+
+            m_pobFollowedNode = followedNode;
+            m_bBoundarySet = true;
+            m_bBoundaryFullyCovered = false;
+
+            CCSize winSize = CCDirector.sharedDirector().getWinSize();
+            m_obFullScreenSize = new CCPoint(winSize.width, winSize.height);
+            m_obHalfScreenSize = CCPointExtension.ccpMult(m_obFullScreenSize, 0.5f);
+
+            m_fLeftBoundary = -((rect.origin.x + rect.size.width) - m_obFullScreenSize.x);
+            m_fRightBoundary = -rect.origin.x;
+            m_fLeftBoundary = -rect.origin.y;
+            m_fBottomBoundary = -((rect.origin.y + rect.size.height) - m_obFullScreenSize.y);
+
+            if (m_fRightBoundary < m_fLeftBoundary)
+            {
+                // screen width is larger than world's boundary width
+                //set both in the middle of the world
+                m_fRightBoundary = m_fLeftBoundary = (m_fLeftBoundary + m_fRightBoundary) / 2;
+            }
+            if (m_fTopBoundary < m_fBottomBoundary)
+            {
+                // screen width is larger than world's boundary width
+                //set both in the middle of the world
+                m_fTopBoundary = m_fBottomBoundary = (m_fTopBoundary + m_fBottomBoundary) / 2;
+            }
+
+            if ((m_fTopBoundary == m_fBottomBoundary) && (m_fLeftBoundary == m_fRightBoundary))
+            {
+                m_bBoundaryFullyCovered = true;
+            }
+
+            return true;
+        }
+
+        public bool initWithTarget(CCNode followedNode)
+        {
+            Debug.Assert(followedNode != null);
+
+            m_pobFollowedNode = followedNode;
+            m_bBoundarySet = false;
+            m_bBoundaryFullyCovered = false;
+
+            CCSize winSize = CCDirector.sharedDirector().getWinSize();
+            m_obFullScreenSize = new CCPoint(winSize.width, winSize.height);
+            m_obHalfScreenSize = CCPointExtension.ccpMult(m_obFullScreenSize, 0.5f);
+
+            return true;
+        }
+
+        public override CCObject copyWithZone(CCZone zone)
+        {
+            CCZone tempZone = zone;
+            CCFollow ret = null;
+            if (tempZone != null && tempZone.m_pCopyObject != null)
+            {
+                ret = (CCFollow)tempZone.m_pCopyObject;
+            }
+            else
+            {
+                ret = new CCFollow();
+                tempZone = new CCZone(ret);
+            }
+
+            base.copyWithZone(tempZone);
+            ret.m_nTag = m_nTag;
+
+            return ret;
+        }
+
+        public override void step(float dt)
+        {
+            if (m_bBoundarySet)
+            {
+                // whole map fits inside a single screen, no need to modify the position - unless map boundaries are increased
+                if (m_bBoundaryFullyCovered)
+                {
+                    return;
+                }
+
+                CCPoint tempPos = CCPointExtension.ccpSub(m_obHalfScreenSize, m_pobFollowedNode.position);
+                m_pTarget.position = CCPointExtension.ccp(CCPointExtension.clampf(tempPos.x, m_fLeftBoundary, m_fRightBoundary),
+                                                          CCPointExtension.clampf(tempPos.y, m_fBottomBoundary, m_fTopBoundary));
+            }
+            else
+            {
+                m_pTarget.position = CCPointExtension.ccpSub(m_obHalfScreenSize, m_pobFollowedNode.position);
+            }
+        }
+
+        public override bool isDone()
+        {
+            return !m_pobFollowedNode.isRunning;
+        }
+
+        public override void stop()
+        {
+            m_pTarget = null;
+            base.stop();
+        }
+
+        /** creates the action with no boundary set */
+        public static CCFollow actionWithTarget(CCNode followedNode)
+        {
+            CCFollow ret = new CCFollow();
+
+            if (ret != null && ret.initWithTarget(followedNode))
+            {
+                return ret;
+            }
+
+            return null;
+        }
+
+        /** creates the action with a set boundary */
+        public static CCFollow actionWithTarget(CCNode followedNode, CCRect rect)
+        {
+            CCFollow ret = new CCFollow();
+
+            if (ret != null && ret.initWithTarget(followedNode, rect))
+            {
+                return ret;
+            }
+
+            return null;
+        }
+
+        // node to follow
+        protected CCNode m_pobFollowedNode;
+
+        // whether camera should be limited to certain area
+        protected bool m_bBoundarySet;
+        public bool boundarySet
+        {
+            get
+            {
+                return m_bBoundarySet;
+            }
+            set
+            {
+                m_bBoundarySet = value;
+            }
+        }
+
+        // if screen size is bigger than the boundary - update not needed        
+        protected bool m_bBoundaryFullyCovered;
+
+        // fast access to the screen dimensions
+        protected CCPoint m_obHalfScreenSize;
+        protected CCPoint m_obFullScreenSize;
+
+        // world boundaries
+        protected float m_fLeftBoundary;
+        protected float m_fRightBoundary;
+        protected float m_fTopBoundary;
+        protected float m_fBottomBoundary;
     }
 }
