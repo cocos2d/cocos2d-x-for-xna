6bcda530a2d1dcb745766b22d10f801bb953aabf
Garfield-you <youyang@openxlive.com>
2011-11-07 10:00:28 +0000

    Add CCActionEase.cs

--- cocos2d-xna/actions/action_intervals/action_ease/CCActionEase.cs
+++ cocos2d-xna/actions/action_intervals/action_ease/CCActionEase.cs
@@ -1,3 +1,30 @@
+/****************************************************************************
+Copyright (c) 2010-2011 cocos2d-x.org
+Copyright (c) 2008-2009 Jason Booth
+Copyright (c) 2011      Fulcrum Mobile Network, Inc.
+
+http://www.cocos2d-x.org
+http://www.openxlive.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,5 +34,48 @@ namespace cocos2d
 {
     public class CCActionEase:CCActionInterval
     {
+      /// <summary>
+      /// initializes the action
+      /// </summary>
+      /// <param name="pAction"></param>
+      /// <returns></returns>
+        public bool initWithAction(CCActionInterval pAction)
+        {
+            throw new NotImplementedException();
+        }
+
+      public virtual CCObject copyWithZone(CCZone pZone)
+      {
+          throw new NotImplementedException();
+      }
+      public virtual void startWithTarget(CCNode pTarget)
+      {
+          throw new NotImplementedException();
+      }
+	  public virtual void stop()
+      {
+          throw new NotImplementedException();
+      }
+	  public virtual void update(float time)
+      {
+          throw new NotImplementedException();
+      }
+      public virtual CCActionInterval reverse()
+      {
+          throw new NotImplementedException();
+      }
+
+      /// <summary>
+     /// creates the action
+     /// </summary>
+     /// <param name="pAction"></param>
+     /// <returns></returns>
+	 public static CCActionEase actionWithAction(CCActionInterval pAction)
+     {
+        throw new NotImplementedException();
+     }
+
+     protected CCActionInterval m_pOther;
+
     }
 }
