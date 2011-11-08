89bd37a6d43aca2fd3cc5d544dba2d2ef7e374cf
flyingpacer <flyingpacer@gmail.com>
2011-11-08 08:50:57 +0000

    fixed #810 implement CCFadeTo

--- cocos2d-xna/actions/action_intervals/CCFadeTo.cs
+++ cocos2d-xna/actions/action_intervals/CCFadeTo.cs
@@ -28,6 +28,71 @@ namespace cocos2d
 {
     public class CCFadeTo : CCActionInterval
     {
+        public bool initWithDuration(float duration, byte opacity)
+        {
+            if (base.initWithDuration(duration))
+            {
+                m_toOpacity = opacity;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override CCObject copyWithZone(CCZone pZone)
+        {
+            CCZone pNewZone = null;
+            CCFadeTo pCopy = null;
+            if(pZone != null && pZone.m_pCopyObject != null) 
+            {
+                //in case of being called at sub class
+                pCopy = (CCFadeTo)(pZone.m_pCopyObject);
+            }
+            else
+            {
+                pCopy = new CCFadeTo();
+                pZone = pNewZone = new CCZone(pCopy);
+            }
+
+            base.copyWithZone(pZone);
+
+            pCopy.initWithDuration(m_fDuration, m_toOpacity);
+	
+            return pCopy;
+        }
+
+        public override void startWithTarget(CCNode pTarget)
+        {
+            base.startWithTarget(pTarget);
+
+            CCRGBAProtocol pRGBAProtocol = pTarget as CCRGBAProtocol;
+            if (pRGBAProtocol != null)
+            {
+                m_fromOpacity = pRGBAProtocol.Opacity;
+            }
+        }
+
+        public override void update(float time)
+        {
+            CCRGBAProtocol pRGBAProtocol = m_pTarget as CCRGBAProtocol;
+            if (pRGBAProtocol != null)
+            {
+                pRGBAProtocol.Opacity = (byte)(m_fromOpacity + (m_toOpacity - m_fromOpacity) * time);
+            }
+        }
+
+        /** creates an action with duration and opacity */
+        public static CCFadeTo actionWithDuration(float duration, byte opacity)
+        {
+            CCFadeTo pFadeTo = new CCFadeTo();
+            pFadeTo.initWithDuration(duration, opacity);
+
+             return pFadeTo;
+        }
+
+
+    protected byte m_toOpacity;
+    protected byte m_fromOpacity;
 
     }
 }
