c01663b00aa0d5fac08dbf10c88223a49a38d9dc
minggo <ming@cocos2d-x.org>
2011-11-09 02:56:39 +0000

    fixed #831: implement CCJumpTo

---
+++ cocos2d-xna/actions/action_intervals/CCJumpTo.cs
@@ -0,0 +1,72 @@
+/****************************************************************************
+Copyright (c) 2010-2011 cocos2d-x.org
+Copyright (c) 2008-2011 Ricardo Quesada
+Copyright (c) 2011 Zynga Inc.
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+namespace cocos2d
+{
+    /** @brief Moves a CCNode object to a parabolic position simulating a jump movement by modifying it's position attribute.*/
+    public class CCJumpTo : CCJumpBy
+    {
+        public static CCJumpTo actionWithDuration(float duration, CCPoint position, float height, uint jumps)
+        {
+            CCJumpTo ret = new CCJumpTo();
+            ret.initWithDuration(duration, position, height, jumps);
+
+            return ret;
+        }
+
+        public override void startWithTarget(CCNode target)
+        {
+            base.startWithTarget(target);
+            m_delta = CCPointExtension.ccp(m_delta.x - m_startPosition.x, m_delta.y - m_startPosition.y);
+        }
+
+        public override CCObject copyWithZone(CCZone zone)
+        {
+            CCZone tmpZone = zone;
+            CCJumpTo ret = null;
+
+            if (tmpZone != null && tmpZone.m_pCopyObject != null)
+            {
+                ret = tmpZone.m_pCopyObject as CCJumpTo;
+                if (ret == null)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                ret = new CCJumpTo();
+                tmpZone = new CCZone(ret);
+            }
+
+            base.copyWithZone(tmpZone);
+
+            ret.initWithDuration(m_fDuration, m_delta, m_height, m_nJumps);
+
+            return ret;
+        }
+    }
+}
