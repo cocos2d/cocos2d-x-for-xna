19e0bd537ed2c168c2b470c903d2df0a78feb57a
flyingpacer <flyingpacer@gmail.com>
2011-11-15 07:52:24 +0000

    issue #859 Implement testbasic

---
+++ tests/tests/classes/testBasic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cocos2d;
+
+namespace tests
+{
+    public class TestScene : CCScene
+    {
+
+
+
+    }
+}
