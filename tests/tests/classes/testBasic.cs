0a1ceb6d3e563313694d74d592c3ed3a4376d146
flyingpacer <flyingpacer@gmail.com>
2011-11-16 06:35:11 +0000

    fixed #859 implement testBasic

--- tests/tests/classes/testBasic.cs
+++ tests/tests/classes/testBasic.cs
@@ -6,10 +6,56 @@ using cocos2d;
 
 namespace tests
 {
-    public class TestScene : CCScene
+    public abstract class TestScene : CCScene
     {
+        public TestScene()
+        {
+            m_bPortrait = false;
+            base.init();
+        }
+        
+        public TestScene(bool bPortrait)
+        {
+            m_bPortrait = bPortrait;
+            if (m_bPortrait)
+            {
+                CCDirector.sharedDirector().deviceOrientation = ccDeviceOrientation.CCDeviceOrientationLandscapeRight;
+            }
+    
+            base.init();
+        }
 
+        ~TestScene()
+        {
+        }
 
+        public override void onEnter()
+        {
+            base.onEnter();
 
+            ////add the menu item for back to main menu
+            //CCLabelTTF label = CCLabelTTF.labelWithString("MainMenu", "Arial", 20);
+            //CCMenuItemLabel pMenuItem = CCMenuItemLabel.itemWithLabel(label, this, new SEL_MenuHandler(MainMenuCallback));
+
+            //CCMenu pMenu =CCMenu.menuWithItems(pMenuItem);
+            //CCSize s = CCDirector.sharedDirector().getWinSize();
+            //pMenu.position = new CCPoint(0.0f, 0.0f);
+            //pMenuItem.position = new CCPoint( s.width - 50, 25);
+
+            //addChild(pMenu, 1);
+        }
+
+        public virtual void MainMenuCallback(CCObject pSender)
+        {
+            CCScene pScene = CCScene.node();
+            CCLayer pLayer = new TestController();
+
+            pScene.addChild(pLayer);
+            CCDirector.sharedDirector().replaceScene(pScene);
+        }
+
+        public abstract void runThisTest();
+
+        protected bool m_bPortrait; // indicate if this test case requires portrait mode
     }
 }
