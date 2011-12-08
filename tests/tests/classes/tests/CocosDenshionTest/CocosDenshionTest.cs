50b585a280c641ef02b1b9242a50e6e4cbc4234e
flyingpacer <flyingpacer@gmail.com>
2011-12-08 07:34:56 +0000

    issue #905 add CocosDenshionTest files

---
+++ tests/tests/classes/tests/CocosDenshionTest/CocosDenshionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+using cocos2d;
+using CocosDenshion;
+
+namespace tests 
+{
+    public class CocosDenshionTest : CCLayer
+    {
+
+    }
+
+
+    public class CocosDenshionTestScene : TestScene
+    {
+        public override void runThisTest()
+        {
+	        CCLayer pLayer = new CocosDenshionTest();
+	        addChild(pLayer);
+
+	        CCDirector.sharedDirector().replaceScene(this);
+        }
+    }
+}
