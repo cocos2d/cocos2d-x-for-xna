efa251b1bd7a126791ec60d7b351f526fa7b692f
TotallyEvil Developers <dev@totallyevil.com>
2012-10-18 22:51:33 -0700

    added code to manage the restore state
    For zune/wp7 validation you need to restore the playback state. This
    code change allows you to capture the current playback state and then
    play songs in your game followed by automatic restoration of the
    playback state of the device.

--- CocosDenshion/MusicPlayer.cs
+++ CocosDenshion/MusicPlayer.cs
@@ -7,36 +7,75 @@ using cocos2d;
 
 namespace CocosDenshion
 {
+    /// <summary>
+    /// This interface controls the media player on the device. For Microsoft mobile devices
+    /// that play music, e.g. Zune and phone, you must not intefere with the background music
+    /// unless the user has allowed it.
+    /// </summary>
     public class MusicPlayer
     {
         public static ulong s_mciError;
 
-        uint m_nSoundID;
-        Song m_music;
+        private uint m_nSoundID;
+        private Song m_music;
+        /// <summary>
+        /// Track if we did play our own game song, otherwise the media player is owned
+        /// by the user of the device and that user is listening to background music.
+        /// </summary>
+        private bool m_didPlayGameSong = false;
+        private Song m_SongToPlayAfterClose;
+        private float m_VolumeAfterClose = 1f;
+        private TimeSpan m_PlayPositionAfterClose = TimeSpan.Zero;
+        private MediaQueue m_QueueAfterClose;
+        private bool m_IsRepeatingAfterClose = false;
+        private bool m_IsShuffleAfterClose = false;
 
         public MusicPlayer()
         {
             m_nSoundID = 0;
+            if (MediaPlayer.State == MediaState.Playing)
+            {
+                SaveMediaState();
+            }
+        }
+
+        public void SaveMediaState()
+        {
+            // User is playing a song, so remember the song state.
+            m_SongToPlayAfterClose = MediaPlayer.Queue.ActiveSong;
+            m_VolumeAfterClose = MediaPlayer.Volume;
+            m_PlayPositionAfterClose = MediaPlayer.PlayPosition;
+            m_IsRepeatingAfterClose = MediaPlayer.IsRepeating;
+            m_IsShuffleAfterClose = MediaPlayer.IsShuffled;
+        }
+
+        public void RestoreMediaState()
+        {
+            if (m_SongToPlayAfterClose != null && m_didPlayGameSong)
+            {
+                MediaPlayer.IsShuffled = m_IsShuffleAfterClose;
+                MediaPlayer.IsRepeating = m_IsRepeatingAfterClose;
+                MediaPlayer.Volume = m_VolumeAfterClose;
+                MediaPlayer.Play(m_SongToPlayAfterClose);
+            }
         }
 
         ~MusicPlayer()
         {
             Close();
+            RestoreMediaState();
         }
 
         public void Open(string pFileName, uint uId)
         {
-            do 
-            {
-                if (null == pFileName || pFileName.Length == 0)
-                    break;
+            if (null == pFileName || pFileName.Length == 0)
+                return;
 
-                Close();
+            Close();
 
-                m_music = CCApplication.sharedApplication().content.Load<Song>(pFileName);
-                
-                m_nSoundID = uId;
-            } while (false);
+            m_music = CCApplication.sharedApplication().content.Load<Song>(pFileName);
+
+            m_nSoundID = uId;
         }
 
         public void Play(bool bLoop)
@@ -45,8 +84,9 @@ namespace CocosDenshion
             {
                 MediaPlayer.IsRepeating = bLoop;
                 MediaPlayer.Play(m_music);
+                m_didPlayGameSong = true;
             }
-}
+        }
 
         public void Play()
         {
@@ -55,45 +95,83 @@ namespace CocosDenshion
 
         public void Close()
         {
-#if !ZUNE
-            // Pedro Kayatt - On the Zune, you must restore the play state of background music to the Zune's current music.
-            // This code will prevent your game from being authorized on the windows marketplace.
-            if (IsPlaying())
+            if (IsPlaying() && m_didPlayGameSong)
             {
                 Stop();
             }
-#endif
+
             if (m_music != null)
             {
                 m_music = null;
             }
         }
 
+        /// <summary>
+        /// Pauses the current song being played. 
+        /// </summary>
         public void Pause()
         {
             MediaPlayer.Pause();
         }
 
+        /// <summary>
+        /// Resumes playback of the current song.
+        /// </summary>
         public void Resume()
         {
             MediaPlayer.Resume();
         }
 
+        /// <summary>
+        /// Stops playback of the current song and resets the playback position to zero.
+        /// </summary>
         public void Stop()
         {
             MediaPlayer.Stop();
         }
 
+        /// <summary>
+        /// resets the playback of the current song to its beginning.
+        /// </summary>
         public void Rewind()
         {
+            Song s = MediaPlayer.Queue.ActiveSong;
+
             Stop();
 
             if (null != m_music)
             {
                 MediaPlayer.Play(m_music);
             }
+            else if (s != null)
+            {
+                MediaPlayer.Play(s);
+            }
         }
 
+        /// <summary>
+        /// Returns true if one of the game songs is playing.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPlayingMySong()
+        {
+            if (!m_didPlayGameSong)
+            {
+                return (false);
+            }
+            if (MediaState.Playing == MediaPlayer.State)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if any song is playing in the media player, even if it is not one
+        /// of the songs in the game.
+        /// </summary>
+        /// <returns></returns>
         public bool IsPlaying()
         {
             if (MediaState.Playing == MediaPlayer.State)
@@ -116,7 +194,7 @@ namespace CocosDenshion
                 return MediaPlayer.Volume;
             }
 
-            set 
+            set
             {
                 if (value >= 0.0f && value <= 1.0f)
                 {
