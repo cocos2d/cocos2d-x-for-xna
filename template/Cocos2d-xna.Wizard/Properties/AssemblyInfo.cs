bfea52734b5fffc6b68c30400e5d0fee07925183
TotallyEvil Developers <dev@totallyevil.com>
2012-11-11 22:19:27 -0800

    GUID ref update for VS2012.
    [Bob White] Changed how the GUID is written to the generated code to use
    a programmatic variable instead of a hard coded GUID. This is necessary
    for VS2012.

--- template/Cocos2d-xna.Wizard/Properties/AssemblyInfo.cs
+++ template/Cocos2d-xna.Wizard/Properties/AssemblyInfo.cs
@@ -20,7 +20,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("69df5bbe-3b22-4519-9c27-4869c8f0f121")]
+[assembly: Guid("$guid1$")]
+// [assembly: Guid("69df5bbe-3b22-4519-9c27-4869c8f0f121")]
 
 // Version information for an assembly consists of the following four values:
 //
